348936fca37da2297959394ba9301af67d507d8b
Marzzzo <marco.burdick@web.de>
2025-06-19 12:45:32 +0200

    update index and style

--- js/game.js
+++ js/game.js
@@ -5,3 +5,35 @@ function init() {
   canvas = document.getElementById('canvas');
   world = new World(canvas);
 }
+
+function openFullscreen() {
+  let fullscreen = document.getElementById('fullscreen');
+  let headline = document.getElementById('headline');
+  headline.classList.add('d-none');
+  enterFullscreen(fullscreen);
+}
+
+function closeFullscreen() {
+  let fullscreen = document.getElementById('fullscreen');
+  let headline = document.getElementById('headline');
+  headline.classList.remove('d-none');
+  exitFullscreen(fullscreen);
+}
+
+function enterFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
