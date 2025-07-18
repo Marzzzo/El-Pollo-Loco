b6dfc47571270440a34abf7ab98f00a4771cdf5c
Marzzzo <marco.burdick@web.de>
2025-07-18 17:31:49 +0200

    added hurt and dead Images.
    when hit goes Energy down and Show Images for hit.
    Is the Energy = 0 , Show the dead Images.

--- models/character.class.js
+++ models/character.class.js
@@ -60,6 +60,22 @@ class Character extends MoveableObjects {
     'img/2_character_pepe/3_jump/J-39.png',
   ];
 
+  IMAGES_DEAD = [
+    'img/2_character_pepe/5_dead/D-51.png',
+    'img/2_character_pepe/5_dead/D-52.png',
+    'img/2_character_pepe/5_dead/D-53.png',
+    'img/2_character_pepe/5_dead/D-54.png',
+    'img/2_character_pepe/5_dead/D-55.png',
+    'img/2_character_pepe/5_dead/D-56.png',
+    'img/2_character_pepe/5_dead/D-57.png',
+  ];
+
+  IMAGES_HURT = [
+    'img/2_character_pepe/4_hurt/H-41.png',
+    'img/2_character_pepe/4_hurt/H-42.png',
+    'img/2_character_pepe/4_hurt/H-43.png',
+  ];
+
   constructor() {
     super();
     this.loadImage(this.IMAGES_WALKING[0]);
@@ -67,6 +83,8 @@ class Character extends MoveableObjects {
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_IDLE);
     this.loadImages(this.IMAGES_LONGIDLE);
+    this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_HURT);
     this.applyGravity();
     this.animate();
   }
@@ -77,7 +95,6 @@ class Character extends MoveableObjects {
         this.otherDirection = false;
         this.moveRight();
       }
-
       if (this.world.keyboard.LEFT && this.x > -650) {
         this.otherDirection = true;
         this.moveLeft();
@@ -85,18 +102,17 @@ class Character extends MoveableObjects {
       if (this.world.keyboard.SPACE && !this.isAboveGround()) {
         this.jump();
       }
-
       this.cameraTracking();
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.Right || this.world.keyboard.LEFT) {
+      if (this.isDead()) {
+        this.playAnimation(this.IMAGES_DEAD);
+      } else if (this.isHurt()) {
+        this.playAnimation(this.IMAGES_HURT);
+      } else if (this.world.keyboard.Right || this.world.keyboard.LEFT) {
         this.playAnimation(this.IMAGES_WALKING);
-      }
-    }, 80);
-
-    setInterval(() => {
-      if (this.world.keyboard.SPACE) {
+      } else if (this.world.keyboard.SPACE) {
         this.playAnimation(this.IMAGES_JUMPING);
       }
     }, 80);
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -10,6 +10,8 @@ class MoveableObjects {
   otherDirection = false;
   speedY = 0;
   acceleration = 2.5;
+  energy = 100;
+  lastHit = 0;
 
   offset = {
     top: 7,
@@ -52,13 +54,42 @@ class MoveableObjects {
       ctx.beginPath();
       ctx.lineWidth = '1';
       ctx.strokeStyle = 'red';
-      ctx.rect(this.x + this.offset.left, this.y + this.offset.top, this.width - this.offset.right, this.height - this.offset.bottom);
+      ctx.rect(
+        this.x + this.offset.left,
+        this.y + this.offset.top,
+        this.width - this.offset.right,
+        this.height - this.offset.bottom
+      );
       ctx.stroke();
     }
   }
 
+  hit() {
+    this.energy -= 2;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
+    }
+  }
+
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000;
+    return timepassed < 1;
+  }
+
+  isDead() {
+    return this.energy == 0;
+  }
+
   isColliding(mo) {
-    return this.x + this.width - this.offset.right > mo.x && this.y + this.height > mo.y && this.x < mo.x && this.y < mo.y + mo.height;
+    return (
+      this.x + this.width - this.offset.right > mo.x &&
+      this.y + this.height > mo.y &&
+      this.x < mo.x &&
+      this.y < mo.y + mo.height
+    );
   }
 
   loadImages(arr) {
@@ -70,7 +101,7 @@ class MoveableObjects {
   }
 
   playAnimation(images) {
-    let i = this.currentImage % this.IMAGES_WALKING.length;
+    let i = this.currentImage % images.length;
     let path = images[i];
     this.img = this.imageCache[path];
     this.currentImage++;
